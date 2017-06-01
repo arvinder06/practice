d5fff1e9a99f4ad24f21b6deb6d2e2bc731eec4b
arvinder <arvinder@arvinder-ubuntu>
2017-06-02 00:53:46 +1000

    first Commit

---
+++ learning/src/test/java/arviLearning/learning/practiceClass.java
@@ -0,0 +1,21 @@
+package arviLearning.learning;
+import java.net.MalformedURLException;
+import java.net.URL;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class practiceClass {
+
+	public static void main(String[] args) throws MalformedURLException {
+		WebDriver driver=null;
+		DesiredCapabilities capability=DesiredCapabilities.chrome();
+		capability.setBrowserName("chrome");
+		capability.setPlatform(Platform.LINUX);
+
+		driver=new RemoteWebDriver(new URL("http://172.17.0.1:4444/wd/hub"),capability);
+		driver.get("http://www.google.com");
+	}
+
+}
