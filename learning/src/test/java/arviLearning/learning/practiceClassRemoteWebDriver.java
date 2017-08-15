d2f0ce7a2eecdc7c05f6f57e06936d2f9638a141
arvinder <arvinder@192-168-1-2.tpgi.com.au>
2017-08-16 02:17:25 +1000

    Adding docker url and changes

--- learning/src/test/java/arviLearning/learning/practiceClassRemoteWebDriver.java
+++ learning/src/test/java/arviLearning/learning/practiceClassRemoteWebDriver.java
@@ -9,13 +9,20 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 public class practiceClassRemoteWebDriver {
 
 	public static void main(String[] args) throws MalformedURLException {
+		
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.home")+"/eclipse-workspace/maven.1502674278044/learning/src/test/java/chromedriver");
+
 		WebDriver driver=null;
 		DesiredCapabilities capability=DesiredCapabilities.chrome();
 		capability.setBrowserName("chrome");
-		capability.setPlatform(Platform.LINUX);
+		capability.setPlatform(Platform.ANY);
 
-		driver=new RemoteWebDriver(new URL("http://172.17.0.1:4444/wd/hub"),capability);
+		driver=new RemoteWebDriver(new URL("http://localhost:5000//wd/hub"),capability);
+		System.out.println("*********Execution Started*********");
+		System.out.println("Launched browser successfully");
 		driver.get("http://www.google.com");
+		System.out.println("Launched browser successfully");
+		driver.quit();
 	} 
 
 }
