665e037f5b8895bb966c5562c5a9f9bb1bed87be
ArvinderS <ArvinderS@syd-d-pc0fxy2n.revit.local>
2019-02-08 11:50:01 +1100

    POM update

--- learning/src/test/java/arviLearning/practiceChromeDriver.java
+++ learning/src/test/java/arviLearning/practiceChromeDriver.java
@@ -1,25 +1,23 @@
 package arviLearning;
 
-import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
 
 public class practiceChromeDriver {
 
 	public static void main(String[] args) {
+		
 		String usrdir=System.getProperty("user.dir");
 		usrdir=usrdir+"\\src\\test\\resources";
 		System.setProperty("webdriver.chrome.driver",usrdir+"\\chromedriverWindows.exe");		
 		 WebDriver driver=new ChromeDriver();
 		 
 		 driver.get("https://www.google.com");
+		 driver.manage().window().maximize();
 
 //		System.setProperty("webdriver.ie.driver", userDir + "\\src\\test\\resources\\IEDriverServer.exe");
 //		WebDriver driver = new InternetExplorerDriver();
-//		driver.get("www.google.com");
-		int initial_size = driver.manage().window().getSize().getWidth();
-		System.out.println(initial_size);
+//		driver.get("www.google.com");		
 	}
 
 }
